ac1185a266bcb1838b26b961a62f23146c2746dc
Sean McElroy <seanpetermcelroy@gmail.com>
2022-08-31 22:37:03 +0100

    added theme icon switching effect

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 "strict";
 
-let btn = document.getElementById("but");
+let btn = document.getElementsByClassName("icon-box")[0];
+let moon = document.getElementById("moon_svg")
+let sun = document.getElementById("sun_svg")
 const body =document.body;
 
 btn.onclick= ()=>{
     if (body.classList[0]=="dark"){
         body.classList.replace("dark", "light")
+        sun.classList.add("hidden")
+        moon.classList.remove("hidden")
     }else{
         body.classList.replace("light","dark")
+        moon.classList.add("hidden")
+        sun.classList.remove("hidden")
 
     }
 }
+
