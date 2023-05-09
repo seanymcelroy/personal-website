038dba8b8425d31e9b2c1793c861e99403ca1cb3
Sean McElroy <seanpetermcelroy@gmail.com>
2023-05-09 22:53:15 +0100

    more research needed. occurs when card is full width

--- app.js
+++ app.js
@@ -73,82 +73,12 @@ document.addEventListener('DOMContentLoaded', function() {
   selfCode();
 
 
-  function typeWriter(text, target, blinkerClass, callback) {
-      let i = 0;
-      blinker.classList.remove("hidden");
-      blinker.classList.add("blinker-animate");
-      function type() {
-        if (i < text.length) {
-          let char = text.charAt(i);
-          let styledChar = '';
-    
-          if (target === 'programmer-text-2' && i < 6) {
-            styledChar = `<span class="tag attr-name token">${char}</span>`;
-          } else {
-              styledChar = `</span><span class="tag attr-value token">${char}</span>`;
-          }
-    
-          document.getElementById(target).innerHTML += styledChar;
-          i++;
-          setTimeout(type, speed);
-        } else {
-          blinker.classList.add("blinker-animate");
-          setTimeout(()=>{
-              blinker.classList.add("hidden");
-              if (callback) {
-                  callback();
-              }
-          }, 2500)
-      }
-    }
-  }
-
-  // setTimeout(()=>
-  //     {
-  //         blinker.classList.remove("blinker-animate");
-  //         type();
-  //     }, 2000)
-    
-    
-  //   function showGreen() {
-  //     const programmerText = document.getElementById("programmer-green");
-  //     programmerText.classList.remove("programmer-before");
-  //     programmerText.classList.add("text-hacker-green");
-  //     setTimeout(() => typeWriter(blueText, 'programmer-text-2', "blinker-blue", displayBlue), 2000);
-  //   }
-    
-    // function displayBlue() {
-    //   document.getElementById("mcelroy").classList.add("blue");
-    // }
-    
-    // setTimeout(() => typeWriter(hackerGreenText, 'programmer-text', "blinker-green", showGreen), 1000);
-    
-
   // Get elements
   const card = document.querySelector(".card-body");
-  const modal = document.getElementById("myModal");
-  const closeBtn = document.querySelector(".close");
 
-  // Show modal when card is clicked
-  card.addEventListener("click", () => {
-      console.log("HEKK")
-    modal.style.display = "block";
-  });
-
-  // Hide modal when close button is clicked
-  closeBtn.addEventListener("click", () => {
-    modal.style.display = "none";
-  });
-
-  // Hide modal when clicking outside of the modal content
-  window.addEventListener("click", (event) => {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  });
 
   const projectTexts = Array.from(document.getElementsByClassName('card-text'));
-  // console.log(projects)
+  console.log(projectTexts)
 
   const observer = new IntersectionObserver((entries)=>{
       entries.forEach((entry)=>{
@@ -161,6 +91,8 @@ document.addEventListener('DOMContentLoaded', function() {
               entry.target.parentNode.parentNode.classList.remove('show')
               entry.target.parentNode.parentNode.classList.add('invisible');
           }
+        }else{
+          console.log(entry)
         }
       });
   });
